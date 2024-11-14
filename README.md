50f0a7a806a141c96f8040bbc63a6931667c14f2
gitesh.mane21@vit.edu <giteshmane013>
2024-11-14 14:26:45 +0530

    first commitx

---
+++ README.md
@@ -0,0 +1,4 @@
+## Coursera  Getting started with Git and Github week 3 final project
+
+### Authorhrs
+gitesh mane
---
+++ simpleinterest.sh
@@ -0,0 +1,4 @@
+echo # Code of Conduct > CODE_OF_CONDUCT.md
+echo # Contributing Guidelines > CONTRIBUTING.md
+echo # License Information > LICENSE
+echo # Project README > README.md
